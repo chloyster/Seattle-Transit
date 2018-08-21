4b612b6670399323650cbeec98f7d1fd85e8701a
Chris Gix <chrisgix97@gmail.com>
2018-08-21 11:43:20 -0700

    fixed watch printing error

--- app/index.js
+++ app/index.js
@@ -1,8 +1,43 @@
-import document from "document";
+// Import the messaging module
 import * as messaging from "messaging";
+import document from "document";
 
-// Message is received from companion
-messaging.peerSocket.onmessage = evt => {
-  let myJson = evt.data;
-  console.log("Your closest stop is " + myJson);
-};
+let time = document.getElementById("stopName");
+
+// Request stop data from the companion
+function fetchStops() {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send a command to the companion
+    messaging.peerSocket.send({
+      command: 'getStops'
+    });
+  }
+}
+
+// Display the stop data received from the companion
+function processStopData(data) {
+  console.log("Your closest stop is: " + data.name);
+  time.text = data.name;
+}
+
+// Listen for the onopen event
+messaging.peerSocket.onopen = function() {
+  // Fetch stops when the connection opens
+  fetchStops();
+}
+
+// Listen for messages from the companion
+messaging.peerSocket.onmessage = function(evt) {
+  if (evt.data) {
+    processStopData(evt.data);
+  }
+}
+
+// Listen for the onerror event
+messaging.peerSocket.onerror = function(err) {
+  // Handle any errors
+  console.log("Connection error: " + err.code + " - " + err.message);
+}
+
+// Fetch the weather every 30 minutes
+setInterval(fetchStops, 30 * 1000 * 60);
--- companion/index.js
+++ companion/index.js
@@ -1,35 +1,51 @@
+// Import the messaging module
 import * as messaging from "messaging";
+// Import the Geolocation module
 import { geolocation } from "geolocation";
-// Listen for the onopen event
-//messaging.peerSocket.onopen = function() {
-  //messaging.peerSocket.send("hello");
-  
-//}
 
+var API_KEY = "b3d23c96-9843-4544-b7d4-464a402b7a8d";
+var ENDPOINT = "https://api.pugetsound.onebusaway.org/api/where/stops-for-location.json?key=";
 
-geolocation.getCurrentPosition(function(position){
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
-  
-  let url = 'https://api.pugetsound.onebusaway.org/api/where/stops-for-location.json?key=b3d23c96-9843-4544-b7d4-464a402b7a8d';
+// Fetch the stops from One Bus Away
+function queryOBA(lat, long) {
+  fetch(ENDPOINT + API_KEY + '&lat=' + lat + '&lon=' + long)
+  .then(function (response) {
+      response.json()
+      .then(function(data) {
+        var stop = {
+          name: data["data"]["list"][0]["name"]
+        }
+        // Send the stop data to the device
+        returnStopData(stop);
+      });
+  })
+  .catch(function (err) {
+    console.log("Error fetching stops: " + err);
+  });
+}
 
-  url += '&lat=';
-  url += lat;
-  url += '&lon=';
-  url += long;
-
-  console.log(url);
-  fetchSomething(url);
-});
-
-async function fetchSomething(url) {
-  console.log('starting...');
-  const response = await fetch(url);
-  const responseJSON = await response.json();
-  
-  messaging.peerSocket.onopen = function(){
-    messaging.peerSocket.send(responseJSON.data.list[0].name);
+// Send the stop data to the device
+function returnStopData(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send a command to the device
+    messaging.peerSocket.send(data);
+  } else {
+    console.log("Error: Connection is not open");
   }
 }
 
-//fetchSomething(url);
+// Listen for messages from the device
+messaging.peerSocket.onmessage = function(evt) {
+  if (evt.data && evt.data.command == "getStops") {
+    geolocation.getCurrentPosition(function(position){
+      console.log("Gathered location data");
+      queryOBA(position.coords.latitude, position.coords.longitude);
+    });
+  }
+}
+
+// Listen for the onerror event
+messaging.peerSocket.onerror = function(err) {
+  // Handle any errors
+  console.log("Connection error: " + err.code + " - " + err.message);
+}
